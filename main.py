ca78b124decf1905e2567e63d7afd32912944ac2
Pragun <pragun1106@gmail.com>
2024-08-23 18:44:21 +0530

    Improved Logic
    Introduced safe zones on the board.
    Added a winning condition and a check for the game winner.
    Enhanced player movement with safe zones and finishing positions.
    Added a display_board method to visually show the board state in the console.

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import random
 class Ludo:
     def __init__(self):
         self.board = [0] * 40
-        self.players = [{'position': 0, 'home': True, 'color': color} for color in ['Red', 'Green', 'Blue', 'Yellow']]
+        self.players = [{'position': 0, 'home': True, 'color': color, 'safe_zone': False} for color in ['Red', 'Green', 'Blue', 'Yellow']]
         self.current_player = 0
         self.start_positions = [0, 10, 20, 30]
+        self.safe_zones = set([5, 15, 25, 35]) # Example safe zones
+        self.winning_positions = [39] * 4
+        self.winner = None
 
     def roll_dice(self):
         return random.randint(1, 6)
@@ -19,39 +22,54 @@ class Ludo:
         else:
             new_position = player['position'] + steps
             if new_position < len(self.board):
+                if new_position in self.safe_zones:
+                    player['safe_zone'] = True
+                else:
+                    player['safe_zone'] = False
                 player['position'] = new_position
+                if player['position'] == self.winning_positions[player_index]:
+                    player['home'] = True
+                    player['position'] = 40 # Considered as finished
 
     def check_for_landing(self, player_index):
         player = self.players[player_index]
         for index, other_player in enumerate(self.players):
-            if index != player_index and other_player['position'] == player['position']:
+            if index != player_index and other_player['position'] == player['position'] and not other_player['safe_zone']:
                 other_player['home'] = True
                 other_player['position'] = 0
 
     def play_turn(self):
-        dice_roll = self.roll_dice()
-        self.move_player(self.current_player, dice_roll)
-        self.check_for_landing(self.current_player)
-        self.current_player = (self.current_player + 1) % 4
+        if not self.winner:
+            dice_roll = self.roll_dice()
+            self.move_player(self.current_player, dice_roll)
+            self.check_for_landing(self.current_player)
+            self.check_for_winner()
+            self.current_player = (self.current_player + 1) % 4
+
+    def check_for_winner(self):
+        for player in self.players:
+            if player['position'] == 40:
+                self.winner = player['color']
 
     def get_positions(self):
-        return [(player['color'], player['position']) for player in self.players]
+        return [(player['color'], player['position'], player['safe_zone']) for player in self.players]
 
-    def is_winner(self):
+    def display_board(self):
+        board_display = ['.' for _ in range(40)]
         for player in self.players:
-            if player['position'] == len(self.board) - 1:
-                return player['color']
-        return None
+            if player['position'] < 40:
+                board_display[player['position']] = player['color'][0]
+        print("Board: " + ''.join(board_display))
 
 def main():
     game = Ludo()
     rounds = 0
-    while not game.is_winner():
+    while not game.winner:
         game.play_turn()
+        game.display_board()
         print(f"Round {rounds}: {game.get_positions()}")
         rounds += 1
-    winner = game.is_winner()
-    print(f"The winner is {winner}!")
+    print(f"The winner is {game.winner}!")
 
 if __name__ == "__main__":
     main()
